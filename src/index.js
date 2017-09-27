3a891f3d434589e2ff09e236f80a1e618c898d70
xeontem EPAM <xeontem@gmail.com>
2017-09-27 10:34:02 +0300

    fix: fp resolve

--- src/index.js
+++ src/index.js
@@ -1,63 +1,64 @@
-module.exports = function zeros(blob) {
-//------------------------- basic functions ---------------------------------
-var compose = f => g => x => f(g(x));
-var condF = x => f => g => x() ? f() : g();
+module.exports = function zeros(expression) {
+	const blob = expression;// input value
 
-//----------------------- calculate factorial -----------------------------------------
-var fact = num => num === 1 ? num * 1 : num * fact(num - 1);
+	//------------------------- must have functions ---------------------------------
+	const compose = f => g => x => f(g(x));
+	const condF = x => f => g => x() ? f() : g();
+	const cond = x => t => f => x ? t : f;
 
-//----------------------- calculate mul: 10*8*6*4*2 || 9*7*5*3*1 -----------------------------------------
-var multier = n => condF(x => n > 0)(x => n * multier(n - 2))(x => 1);
+	//---------------------------------------------------------------------------
+	const fact = num => num === 1 ? num * 1 : num * fact(num - 1);// factorial
+	const multier = n => n > 0 ? n * multier(n - 2) : 1;// double factorial
 
-//----------------------- calculate count of entries in string -----------------------------------------
-var valuesCounter = s => v => mem => 
-    condF(x => s.lastIndexOf(v) === s.length-1)
-        (x => mem.push(valuesCounter(s.slice(0, -1))(v)(mem)))
-        (x => mem.length);
+	const factF = f => num => condF(x => num === 1)(x => num * 1)(x => num * f(num - 1));// factory for fact memoization
+	const multierF = f => n => condF(x => n > 0)(x => n * f(n - 2))(x => 1);// factory for double fact memoization
 
-var zerosCounterF = mem => num => {
-    if(num === 1) return mem;
-    var temp = num;
-    while (temp % 5 == 0){
-        mem++
-        temp /= 5;
-    }
-    return zerosCounterF(mem)(num - 1);
-}
+	// exclamsCounter:: number -> string -> string -> number
+	const exclamsCounter = mem => s => v => condF(x => s.lastIndexOf(v) === s.length-1)(x => exclamsCounter(mem + 1)(s.slice(0, -1))(v))(x => mem);// define how many '!' in a string
 
-var zerosCounterDF = num => {
-    var pyat = 0;
-    while (num > 1){
-        
-        var temp = num;
-        while (temp % 5 == 0){
-            pyat++
-            temp /= 5;
-        }
-        num -= 2;
-    }
-    return pyat;
-}
+	// currExclamsCounter:: string -> string -> number
+	const currExclamsCounter = exclamsCounter(0);// currying counter to store memory in scope
+	
+	//-------------------------------- calculate how many zeros in a number -----------------------------------
+	const fifthCounter = mem => num => condF(x=> num <= 1)
+		(x => mem)
+		(x => cond(num % 5)
+			(fifthCounter(mem)(num/5))
+			(fifthCounter(mem+1)(num/5)));// count all cases which common factor is 5
+	
+	const twosCounter = mem => num => cond(num % 2)(mem)(mem+1);// count all cases which common factor is 2
 
-//------------------------------------- memoization -------------------------------------------------
-var memoize = mem => F => num => 
-    condF(x => mem.has(num))
-        (x => mem.get(num))
-        (x => mem.set(num, F(num)).get(num));
+	//counter:: number -> number -> (number -> number) -> number -> number
+	const counter = mem => df => f => num => condF(x => num <= 1)// f - would be a 'fifthCounter' or 'twosCounter' to count zeros
+    (x => mem)// if number is 1 return the count of cases
+    (x => counter(f(mem)(num))(df)(f)(num - df));// recursive call counter
 
-var memFact = num => memoize(new Map())(fact)(num);
-var memMultier = num => memoize(new Map())(multier)(num);
+	// currCounter:: number -> (number -> number) -> number -> number
+	const currCounter = counter(0);// currying counter to store memory in scope
 
-// var blob = '1!!*2!!*3!!*4!!*5!!*6!!*7!!*8!!*9!!*10!!';// input value
+	//-------------------------- memoization ---------------------------
+	const Ymem = mem => F => F(x => condF(z => mem.has(x))
+		(z => mem.get(x))
+		(z => mem.set(x, Ymem(mem)(F)(x)).get(x)));// works from second call, because only one recurssion in the function
+	
+	const factMem = Ymem(new Map())(factF);
+	const multMem = Ymem(new Map())(multierF);
 
-//-------------------------- apply functions ------------------------------------------
-var arr = blob.split('*');// create an array of calculatable elements
-var excs = arr.map(blob => valuesCounter(blob)('!')([]));// calculate count of exclamations in each element
+	//------------------------------ performance check ----------------------------------------
+	const perf = f => x => start => -start.valueOf(f(x)) + performance.now();// valueOf() - not necessary, just one-string hack for computing f(x)
+	// console.log(perf(factMem)(40)(performance.now()));
 
-//------------------------------------ array of calculated factorials and/or multiers ------------------------------- 
-var res = arr.map((blob, i) => condF(x => excs[i] === 2)(x => compose(zerosCounterDF)(parseInt)(blob))(x => compose(zerosCounterF(0))(parseInt)(blob)));
+	//-------------------------- apply functions ------------------------------------------
+	const arr = blob.split('*');// split a string by '*'
+	const excs = arr.map(blob => currExclamsCounter(blob)('!'));// factorial/double factorial check
 
-//------------------------------------------ multiply elements/ counting of zeroes in the number -----------------------------------------------
-var num = res.reduce((acc, num) => acc + num, 0);
-return num;
+	//----------------------------------- calculate zeros in every number --------------------
+	let fifth = arr.map((blob, i) => compose(currCounter(excs[i])(fifthCounter))(parseInt)(blob))// count all matches when number is divided by 5
+	let odds = arr.map((blob, i) => compose(currCounter(excs[i])(twosCounter))(parseInt)(blob))// count all matches when number is divided by 2
+
+	//-------------------- sum of zeroes ------------------------------------------------
+	fifth = fifth.reduce((acc, num) => acc + num, 0);
+	odds = odds.reduce((acc, num) => acc + num, 0);
+
+	return Math.min(fifth, odds);// return minimum value of both cases
 }
